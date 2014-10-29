bc2a7b309da33a1ba0d85d391cf071952737348b
Aki Mäkinen <aki.makinen@outlook.com>
2014-10-29 17:32:20 +0200

    Uncommented update from populate meta

--- backend/lbd_backend/populate_meta.py
+++ backend/lbd_backend/populate_meta.py
@@ -7,21 +7,21 @@ from lbd_backend.LBD_REST_locationdata.models import *
 from RESThandlers.Streetlight import models as SL
 import mongoengine
 
-# req = urllib.urlopen(
-#             'http://tampere.navici.com/tampere_wfs_geoserver/opendata/ows?service=WFS&version=1.0.0&request=GetFeature'
-#             '&typeName=opendata:WFS_KATUVALO&outputFormat=json&srsName=EPSG:4326',
-#             proxies={})
-# jsonitem = json.loads(req.read())
-# itemsinserted = 0
-# itemlist = list()
-# SL.Streetlights.drop_collection()
-# for item in jsonitem["features"]:
-#     fid = item.pop("id")
-#     temp = SL.Streetlights.from_json(json.dumps(item))
-#     temp.feature_id = fid
-#     itemlist.append(temp)
-#     itemsinserted += 1
-# SL.Streetlights.objects().insert(itemlist)
+req = urllib.urlopen(
+            'http://tampere.navici.com/tampere_wfs_geoserver/opendata/ows?service=WFS&version=1.0.0&request=GetFeature'
+            '&typeName=opendata:WFS_KATUVALO&outputFormat=json&srsName=EPSG:4326',
+            proxies={})
+jsonitem = json.loads(req.read())
+itemsinserted = 0
+itemlist = list()
+SL.Streetlights.drop_collection()
+for item in jsonitem["features"]:
+    fid = item.pop("id")
+    temp = SL.Streetlights.from_json(json.dumps(item))
+    temp.feature_id = fid
+    itemlist.append(temp)
+    itemsinserted += 1
+SL.Streetlights.objects().insert(itemlist)
 
 o = SL.Streetlights.objects()
 
