41c547f46aceb66e92e5304d5fd40642b161f5c1
Aki Mäkinen <aki.makinen@outlook.com>
2014-10-30 14:22:09 +0200

    urls.py updated

--- backend/lbd_backend/urls.py
+++ backend/lbd_backend/urls.py
@@ -10,13 +10,13 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', getjson),
+    #url(r'^$', getjson),
     #url(r'^changejson$', changejson),
     # url(r'^api/(?P<collection>\w+)$', changejson),
     # url(r'^api/(?P<collection>\w+)/(?P<resource>\w+)$', resttest),
 
     # REST API
-    url(r'^locationdata/api/(?P<collection>\w+)$', collection),
+    url(r'^locationdata/api/(?P<collection>\w+)/$', collection),
     url(r'^locationdata/api/(?P<collection>\w+)/(?P<resource>(\w|\.)+)$', single_resource),
     url(r'^locationdata/api/(?P<collection>\w+)/near/$', collection_near),
     url(r'^locationdata/api/(?P<collection>\w+)/inarea/$', collection_inarea),
