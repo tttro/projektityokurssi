1bb30dd08b0a6c9b12df3af4bdd4b6d2b3901303
Tero <terotai@gmail.com>
2014-11-13 10:10:44 +0200

    Added some new stuff for web

--- web/app/scripts/services/services.js
+++ web/app/scripts/services/services.js
@@ -21,31 +21,45 @@ dataServices.factory('StreetlightInarea', function($http){
             var retData = null;
             var ne = bounds.getNorthEast();
             var sw = bounds.getSouthWest();
-            $http.get(baseUrl + 'Streetlights/inarea/?xbottomleft='+ne.lat()+'&ybottomleft=+'+ne.lng()+'+&ytopright='+sw.lat()+'&xtopright='+sw.lat())
+            //console.log('Streetlights/inarea/?xbottomleft='+sw.lng()+'&ybottomleft='+sw.lat()+'&ytopright='+ne.lat()+'&xtopright='+ne.lng());
+            $http.get(baseUrl + 'Streetlights/inarea/?xbottomleft='+sw.lng()+'&ybottomleft='+sw.lat()+'&ytopright='+ne.lat()+'&xtopright='+ne.lng(),
+                {headers: {"LBD_LOGIN_HEADER": "SimoSahkari"}}
+            )
                 .success(function(data){
                     callback(data);
                 })
                 .error(function(){
-                    console.log("error:StreetlightInarea", error);
+                    console.log("error:StreetlightInarea");
                 });
             return retData;
         }
     };
 });
 dataServices.factory('StreetlightTest', ['$http','$rootScope', function($http, $rootScope){
+    $http.defaults.headers.common['LBD_LOGIN_HEADER'] = 'SimoSahkari';
     var dataStorage;
+    var config = {
+        withCredentials:false,
+        cache:true,
+        headers:  {'LBD_LOGIN_HEADER' : 'SimoSahkari'}
+    };
+
     return {
 
         fetchData: function(callback) {
+            //$http.defaults.headers.common['LBD_LOGIN_HEADER'] = 'SimoSahkari';
+            http://lbdbackend.ignorelist.com/locationdata/api/Streetlights/WFS_KATUVALO.405172
             dataStorage =
-                $http.get(baseUrl + 'Streetlights/inarea/?xbottomleft=23.63&ybottomleft=61.51&ytopright=61.52&xtopright=23.65',
-                    {cache: true})
+                $http.get(baseUrl + 'Streetlights/inarea/?xbottomleft=23.63&ybottomleft=61.51&ytopright=61.52&xtopright=23.65')
                 .success(function(data){
                     dataStorage = data;
                     callback(data);
                 })
-                .error(function(){
-                    console.log("error:streetlight", error);
+                .error(function(data,status,header,config){
+                    console.log("error:streetlight " + data);
+                    console.log("error:streetlight " + status);
+                    console.log("error:streetlight " + header);
+                    console.log("error:streetlight " + config);
                 });
 
             return dataStorage;
