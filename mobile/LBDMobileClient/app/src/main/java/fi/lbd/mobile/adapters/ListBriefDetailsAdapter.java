b5ce5d5ca36840ddfadbde16d341924ea2b89661
Osmoosi <ossi.teikari@gmail.com>
2014-11-21 01:53:18 +0200

    Continued list implementation

--- mobile/LBDMobileClient/app/src/main/java/fi/lbd/mobile/adapters/ListBriefDetailsAdapter.java
+++ mobile/LBDMobileClient/app/src/main/java/fi/lbd/mobile/adapters/ListBriefDetailsAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -25,12 +26,13 @@ public class ListBriefDetailsAdapter extends BaseAdapter {
     private final String TYHJA = "Tyhjä";
     private final String LISATIETOJA = "LISÄTIETOJA";
     private final String SIJAINTI = "SIJAINTI";
-    private final int MAX_BRIEF_DETAILS = 2;
+    private final int MAX_BRIEF_DETAILS = 3;
+    private static int fullListHeight = -1;
+    private static int briefListHeight = -1;
     private MapObject object;
     private ArrayList<Map.Entry<String,String>> additionalProperties;
     private Context context;
-    private static boolean expandDetails = false;
-    private static int visibleDetailsCounter = 2;
+    private boolean expandDetails = false;
 
     public void setObject(MapObject mapObject){
         this.object = mapObject;
@@ -38,15 +40,12 @@ public class ListBriefDetailsAdapter extends BaseAdapter {
             Log.d(" number of additional properties _________", ((Integer)object.getAdditionalProperties().size()).toString());
 
             for (Map.Entry<String, String> entry : object.getAdditionalProperties().entrySet()) {
-                Log.d("1. Lisätty additional property" + entry.getKey(), "_________");
-                Log.d("2. Lisätty Additional property" + entry.getValue(), "_________");
                 this.additionalProperties.add(entry);
             }
         }
     }
 
     public ListBriefDetailsAdapter(Context context, MapObject mapObject) {
-        Log.d("*************", "rakentaja kutsuttu");
         this.context = context;
         this.additionalProperties = new ArrayList<Map.Entry<String,String>>();
         setObject(mapObject);
@@ -55,9 +54,7 @@ public class ListBriefDetailsAdapter extends BaseAdapter {
     @Override
     public int getCount() {
         // Location, additional properties
-        Log.d(Integer.toString(this.object.getAdditionalProperties().size() + 1), "_________");
-        //return (this.object.getAdditionalProperties()).size() + 1;
-        return visibleDetailsCounter;
+        return (this.object.getAdditionalProperties()).size() + 1;
     }
 
     @Override
@@ -65,8 +62,7 @@ public class ListBriefDetailsAdapter extends BaseAdapter {
         if (i == 0){
             return object.getPointLocation();
         }
-        //else if (i > 0 && i < getCount() - 1) {
-        else if (i > 0 && i < visibleDetailsCounter) {
+        else if (i > 0 && i < getCount() - 1) {
             return additionalProperties.get(i).getValue();
         }
         return null;
@@ -82,6 +78,8 @@ public class ListBriefDetailsAdapter extends BaseAdapter {
         if(view == null){
             LayoutInflater inflater = ((Activity) this.context).getLayoutInflater();
             view = inflater.inflate(R.layout.listview_double_row, viewGroup, false);
+            ListView list = (ListView)((View)viewGroup.getParent()).findViewById(android.R.id.list);
+            list.setLayoutParams(new LinearLayout.LayoutParams(2000,2000));
         }
         if (i == 0) {
             TextView textViewId = (TextView) view.findViewById(R.id.textViewObjectId);
@@ -90,8 +88,7 @@ public class ListBriefDetailsAdapter extends BaseAdapter {
             TextView textViewLocation = (TextView) view.findViewById(R.id.textViewObjectLocation);
             textViewLocation.setText(object.getPointLocation().toString());
         }
-        else if (i > 0 && i < visibleDetailsCounter){
-            Log.d("*************", "i on"+Integer.toString(i));
+        else if (i > 0 && i < getCount()){
             String key = additionalProperties.get(i-1).getKey();
             if (key == METADATA){
                 key = LISATIETOJA;
@@ -111,14 +108,14 @@ public class ListBriefDetailsAdapter extends BaseAdapter {
                 new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                toggleVisibleDetails(!expandDetails);
+                expandDetails = !expandDetails;
                 ListView list = ((ListView)((View)(view.getParent())).findViewById(android.R.id.list));
                 ViewGroup.LayoutParams params = list.getLayoutParams();
                 if(expandDetails == true) {
-                    params.height = 1000;
+                    params.height = calculateFullListHeight(list);
                 }
                 else {
-                    params.height = 200;
+                    params.height = calculateBriefListHeight(list);
                 }
                 list.setLayoutParams(params);
                 list.requestLayout();
@@ -127,19 +124,44 @@ public class ListBriefDetailsAdapter extends BaseAdapter {
         return view;
     }
 
-    public void setVisibleDetailsCounter(int visibleDetails){
-        visibleDetailsCounter = visibleDetails;
+    public int calculateBriefListHeight(ListView listView){
+        Log.d("22222222222 brieflisttHeight on ", Integer.toString(briefListHeight));
+        Log.d("222222222 calculatebrieflistissä, expandDetails on  ", Boolean.toString(expandDetails));
+
+        if(briefListHeight == -1) {
+            View firstVisible = listView.getChildAt(listView.getFirstVisiblePosition());
+            int height = firstVisible.getHeight();
+            int lastElementPosition = listView.getFirstVisiblePosition() + listView.getCount() - 1;
+
+            for (int i = 1; i < MAX_BRIEF_DETAILS; ++i) {
+                int wantedChild = listView.getFirstVisiblePosition() + i;
+                if (wantedChild > 0 && wantedChild < lastElementPosition) {
+                    height = height + listView.getChildAt(i).getHeight();
+                } else {
+                    briefListHeight = height;
+                    return briefListHeight;
+                }
+            }
+            briefListHeight = height;
+        }
+        return briefListHeight;
     }
 
-    public void toggleVisibleDetails(boolean isExpanded){
-        this.expandDetails = isExpanded;
-        if(expandDetails == true){
-            setVisibleDetailsCounter(this.object.getAdditionalProperties().size() + 1);
-            Log.d("*************", "visible details counter set to"+Integer.toString(getCount()));
+    public int calculateFullListHeight(ListView listView){
+
+        Log.d("22222222222 fullListHeight on ", Integer.toString(fullListHeight));
+        Log.d("22222222222 calculatefulllistissä, expandDetails on  ", Boolean.toString(expandDetails));
+
+        if(fullListHeight == -1) {
+            View firstVisible = listView.getChildAt(listView.getFirstVisiblePosition());
+            int height = firstVisible.getHeight();
+            int startPosition = listView.getFirstVisiblePosition() + 1;
+            int endPosition = listView.getFirstVisiblePosition() + listView.getCount();
+            for (int i = startPosition; i < endPosition; ++i) {
+                height = height + listView.getChildAt(i).getHeight();
+            }
+            fullListHeight = height;
         }
-        else {
-            setVisibleDetailsCounter(MAX_BRIEF_DETAILS);
-            Log.d("*************", "else haara");
-        }
+        return fullListHeight;
     }
 }
