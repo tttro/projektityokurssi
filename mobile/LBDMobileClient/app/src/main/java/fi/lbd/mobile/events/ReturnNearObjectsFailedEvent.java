90683fa908619d68fd8d6f8fcab2ece1f2445f39
Osmoosi <ossi.teikari@gmail.com>
2014-12-02 13:58:04 +0200

    Fixed a bug with ListDetailsAdapter. BackendHandlerService returns an event in case ReturnNearObjects fails. ListObjectsFragment listens to the event.

---
+++ mobile/LBDMobileClient/app/src/main/java/fi/lbd/mobile/events/ReturnNearObjectsFailedEvent.java
@@ -0,0 +1,10 @@
+package fi.lbd.mobile.events;
+
+/**
+ * Created by Ossi on 2.12.2014.
+ */
+public class ReturnNearObjectsFailedEvent extends AbstractEvent {
+
+    public ReturnNearObjectsFailedEvent() {
+    }
+}
