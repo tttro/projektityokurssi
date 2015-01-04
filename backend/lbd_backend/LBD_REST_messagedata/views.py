3891699d3de298ffe342105c503d62504bab4ba2
Aki Mäkinen <aki.makinen@outlook.com>
2015-01-04 14:17:35 +0200

    Messaging fixes

--- backend/lbd_backend/LBD_REST_messagedata/views.py
+++ backend/lbd_backend/LBD_REST_messagedata/views.py
@@ -2,7 +2,8 @@
 import json
 from django.http import HttpResponse
 import mongoengine
-from lbd_backend.LBD_REST_messagedata.models import Message
+from lbd_backend.LBD_REST_locationdata.models import MetaDocument
+from lbd_backend.LBD_REST_messagedata.models import Message, Attachment
 from lbd_backend.LBD_REST_users.models import User
 from lbd_backend.utils import s_codes
 
@@ -10,6 +11,17 @@ __author__ = 'Aki Mäkinen'
 
 from django.views.decorators.http import require_http_methods
 from lbd_backend.LBD_REST_locationdata.decorators import this_is_a_login_wrapper_dummy
+
+@this_is_a_login_wrapper_dummy
+@require_http_methods(["GET", "DELETE"])
+def msg_single(request, *args, **kwargs):
+    userdata = User.objects.get(user_id=kwargs["lbduser"].user_id)
+    if request.method == "GET":
+        item = Message.objects(recipient=userdata.email, mid=kwargs["message"]).as_pymongo()
+        return HttpResponse(content=json.dumps(item), status=s_codes["OK"], content_type="application/json")
+    elif request.method == "DELETE":
+        item = Message.objects(recipient=userdata.email, mid=kwargs["message"]).delete()
+        return HttpResponse(status=s_codes["OK"])
 
 
 @this_is_a_login_wrapper_dummy
@@ -30,7 +42,6 @@ def msg_collection(request, *args, **kwargs):
             item.pop("_id")
             temp.append(item)
         return HttpResponse(content=json.dumps(temp), status=s_codes["OK"], content_type="application/json")
-
     #############################################################
     #
     # DELETE
@@ -55,13 +66,13 @@ def msg_send(request, *args, **kwargs):
     #
     #############################################################
     if request.method == "POST":
+        msg_fields = ["category", "recipient", "attachments", "topic", "message"]
+        att_fields = ["category", "aid"]
+
         body = request.body
         content_json = json.loads(body)
         if not ("recipient" in content_json and "topic" in content_json and "message" in content_json):
             return HttpResponse(status=s_codes["BAD"])
-
-        msg_fields = ["category", "recipient", "attachments", "topic", "message"]
-        att_fields = ["category", "aid"]
 
         del_from_msg = list()
         for field in content_json:
@@ -70,19 +81,29 @@ def msg_send(request, *args, **kwargs):
 
         for item in del_from_msg:
             del content_json[item]
-            print item
 
         del_from_att = list()
         if "attachments" in content_json:
-            for field in content_json["attachments"]:
-                if not ("category" in field or "aid" in field):
+            for item in content_json["attachments"]:
+                if not ("category" in item or "aid" in item):
                     return HttpResponse(status=s_codes["BAD"])
-                if field not in att_fields:
-                    del_from_att.append(field)
+                for fieldname, value in item.iteritems:
+                    if fieldname not in att_fields:
+                        del_from_att.append(fieldname)
+                    else:
+                        try:
+                            MetaDocument.objects.get(feature_id=value)
+                        except mongoengine.DoesNotExist:
+                            return HttpResponse(status=s_codes["BAD"],
+                                                content_json='{"message": "Attachment item not found"}',
+                                                content_type="application/json")
 
         for item in del_from_att:
-            del content_json["attachments"][item]
-            print item
+            try:
+                index = content_json["attachments"].index(item)
+                del content_json["attachments"][index]
+            except ValueError:
+                pass
         content_json["sender"] = userdata.email
         content_json["recipient"] = content_json["recipient"].lower()
         try:
@@ -92,8 +113,11 @@ def msg_send(request, *args, **kwargs):
 
         if True: # PLACEHOLDER
             try:
-                print content_json
+                attlist = list()
+                for att in content_json["attachments"]:
+                    attlist.append(Attachment(**att))
                 temp = Message(**content_json)
+                temp.attachements = attlist
                 temp.save()
             except mongoengine.NotUniqueError:
                 pass
