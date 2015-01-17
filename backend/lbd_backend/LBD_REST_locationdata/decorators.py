bd850ac46c1bdf3a30893ecbaacbf4886a4a92a4
Aki Mäkinen <aki.makinen@outlook.com>
2015-01-18 00:09:37 +0200

    Experimental

--- backend/lbd_backend/LBD_REST_locationdata/decorators.py
+++ backend/lbd_backend/LBD_REST_locationdata/decorators.py
@@ -26,6 +26,16 @@ from RESThandlers.HandlerInterface.Exceptions import CollectionNotInstalled
 from RESThandlers.HandlerInterface.Factory import HandlerFactory
 
 
+_supported_http_methods = ["GET", "PUT", "POST", "DELETE", "OPTIONS"]
+
+def authenticate_only_methods(func, lst):
+    @wraps(func)
+    def wrapper(request, *args, **kwargs):
+        kwargs["authenticate_only_methods"] = lst
+        return func(request, *args, **kwargs)
+    return wrapper
+
+
 def location_collection(func):
     """
     *Wrapper*
@@ -63,52 +73,64 @@ def lbd_require_login(func):
     """
     @wraps(func)
     def wrapper(request, *args, **kwargs):
-        if "HTTP_LBD_LOGIN_HEADER" in request.META and "HTTP_LBD_OAUTH_ID" in request.META:
-            access_token = request.META["HTTP_LBD_LOGIN_HEADER"]
-            userid = request.META["HTTP_LBD_OAUTH_ID"]
-
-            url = 'www.googleapis.com'
-            h = httplib.HTTPSConnection(url)
-            h.request("GET", '/oauth2/v1/tokeninfo?access_token=' + access_token)
-            response = h.getresponse()
-            res_content = response.read()
-            try:
-                result = json.loads(res_content)
-            except ValueError:
-                return HttpResponse(status=s_codes["INTERNALERROR"])
-            if True:
-            #if response.status == 200 and userid == result["user_id"]:
-                print result
-                print response.status
-                print "User matches"
+        if "authenticate_only_methods" in kwargs:
+            for item in kwargs["authenticate_only_methods"]:
+                if item not in _supported_http_methods:
+                    return HttpResponse(status=s_codes["INTERNALERROR"])
+            if request.METHOD in kwargs["authenticate_only_methods"]:
+                require_authentication_headers = True
             else:
-                print "ERRORD!"
-                print "STATUS: " + str(response.status)
-                if "error_description" in result:
-                    errormsg = result["error_description"]
-                else:
-                    errormsg = "Unknown error happened. Oh noes!"
-
-                return HttpResponse(status=s_codes["BAD"], content='{"message": "%s"}' % errormsg,
-                                    content_type="application/json; charset=utf-8")
-
-            try:
-                user = User.objects.get(user_id=request.META["HTTP_LBD_OAUTH_ID"])
-                kwargs["lbduser"] = user
-            except mongoengine.DoesNotExist:
-                return HttpResponse(status=s_codes["UNAUTH"], content_type="application/json; charset=utf-8",
-                                    content='{"message": "Unauthorized"}')
-            except Exception as e:
-                #template = "An exception of type {0} occured. Arguments:\n{1!r}"
-                #print template.format(type(e).__name__, e.args)
-                print e.message
-                return HttpResponse(status=s_codes["INTERNALERROR"], content_type="application/json; charset=utf-8",
-                                    content='{"message": "Internal error: %s"}' % e.message)
-
-            return func(request, *args, **kwargs)
+                require_authentication_headers = False
 
         else:
-            return HttpResponse(status=s_codes["BAD"], content_type="application/json; charset=utf-8",
-                                content='{"message": "Missing header or headers!"}')
+            require_authentication_headers = True
 
+        if require_authentication_headers:
+            if "HTTP_LBD_LOGIN_HEADER" in request.META and "HTTP_LBD_OAUTH_ID" in request.META:
+                access_token = request.META["HTTP_LBD_LOGIN_HEADER"]
+                userid = request.META["HTTP_LBD_OAUTH_ID"]
+
+                url = 'www.googleapis.com'
+                h = httplib.HTTPSConnection(url)
+                h.request("GET", '/oauth2/v1/tokeninfo?access_token=' + access_token)
+                response = h.getresponse()
+                res_content = response.read()
+                try:
+                    result = json.loads(res_content)
+                except ValueError:
+                    return HttpResponse(status=s_codes["INTERNALERROR"])
+                #if True:
+                if response.status == 200 and userid == result["user_id"]:
+                    print result
+                    print response.status
+                    print "User matches"
+                else:
+                    print "ERRORD!"
+                    print "STATUS: " + str(response.status)
+                    if "error_description" in result:
+                        errormsg = result["error_description"]
+                    else:
+                        errormsg = "Unknown error happened. Oh noes!"
+
+                    return HttpResponse(status=s_codes["BAD"], content='{"message": "%s"}' % errormsg,
+                                        content_type="application/json; charset=utf-8")
+
+                try:
+                    user = User.objects.get(user_id=request.META["HTTP_LBD_OAUTH_ID"])
+                    kwargs["lbduser"] = user
+                except mongoengine.DoesNotExist:
+                    return HttpResponse(status=s_codes["UNAUTH"], content_type="application/json; charset=utf-8",
+                                        content='{"message": "Unauthorized"}')
+                except Exception as e:
+                    print e.message
+                    return HttpResponse(status=s_codes["INTERNALERROR"], content_type="application/json; charset=utf-8",
+                                        content='{"message": "Internal error: %s"}' % e.message)
+
+                return func(request, *args, **kwargs)
+
+            else:
+                return HttpResponse(status=s_codes["BAD"], content_type="application/json; charset=utf-8",
+                                    content='{"message": "Missing header or headers!"}')
+        else:
+            return func(request, *args, **kwargs)
     return wrapper
